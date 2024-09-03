99a6d994a5029d771d33a6de486839172c3038bf
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-09-03 10:21:36 -0400

    update 18

---
+++ src/app/services/user-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: "root"})
+export class UserService {
+    userList = [
+        "Drew Wilson",
+        "Elmira Keddy",
+        "Eveline Grandisson",
+        "Berry Wildes",
+        "Quintus Hastings",
+        "Harp Antonignetti",
+        "Vite Playfair",
+        "Noelle Dowears",
+        "Dunoggota Lubbock",
+        "Auberta Skerrett",
+        "Constantin Cosgry",
+        "Loleta Grenfell",
+        "Nadeen Matchett",
+        "Elli Galliver",
+        "Gayla Hawtin",
+        "Liam Antwis",
+        "Merilyn Baumford",
+        "Lilas Colquyte",
+        "Roi Kinworthy",
+        "Patin Flecknoe",
+        "Etienne Vedeneev",
+        "Diane Evesque",
+        "Ashlee Amoore",
+        "Julissa Bandey",
+        "Merridie McPartling",
+        "Nanete Kitlee"        
+      ];
+}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,35 +7,40 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
   testUser = "Test User"
-  userList = [
-    "Tucker Anselm",
-    "Elmira Keddy",
-    "Eveline Grandisson",
-    "Berry Wildes",
-    "Quintus Hastings",
-    "Harp Antonignetti",
-    "Vite Playfair",
-    "Noelle Dowears",
-    "Delcine Lubbock",
-    "Auberta Skerrett",
-    "Constantin Cosgry",
-    "Loleta Grenfell",
-    "Nadeen Matchett",
-    "Elli Galliver",
-    "Gayla Hawtin",
-    "Liam Antwis",
-    "Merilyn Baumford",
-    "Lilas Colquyte",
-    "Roi Kinworthy",
-    "Patin Flecknoe",
-    "Etienne Vedeneev",
-    "Diane Evesque",
-    "Ashlee Amoore",
-    "Julissa Bandey",
-    "Merridie McPartling",
-    "Nanete Kitlee"        
-  ];
+  // userList = [
+  //   "Tucker Anselm",
+  //   "Elmira Keddy",
+  //   "Eveline Grandisson",
+  //   "Berry Wildes",
+  //   "Quintus Hastings",
+  //   "Harp Antonignetti",
+  //   "Vite Playfair",
+  //   "Noelle Dowears",
+  //   "Delcine Lubbock",
+  //   "Auberta Skerrett",
+  //   "Constantin Cosgry",
+  //   "Loleta Grenfell",
+  //   "Nadeen Matchett",
+  //   "Elli Galliver",
+  //   "Gayla Hawtin",
+  //   "Liam Antwis",
+  //   "Merilyn Baumford",
+  //   "Lilas Colquyte",
+  //   "Roi Kinworthy",
+  //   "Patin Flecknoe",
+  //   "Etienne Vedeneev",
+  //   "Diane Evesque",
+  //   "Ashlee Amoore",
+  //   "Julissa Bandey",
+  //   "Merridie McPartling",
+  //   "Nanete Kitlee"        
+  // ];
 
+
+    constructor(
+      
+    ) {}
+    
     removeUser(index: number){
       this.userList.splice(index, 1);
     
