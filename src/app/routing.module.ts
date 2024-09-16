712fce352dbee7d2fcb1d30f15c32fa543958e7e
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-09-16 13:25:02 -0400

    update 19 debugging cont

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,14 +4,14 @@ import { LoginComponent } from "./auth/login/login.component";
 import { UsersComponent } from "./users/users.component";
 // Removed the import statement for AppRoutingModule
 
-const routes: Routes = [
+
     
-    // Add routes for other components here...
-    { path: '', redirectTo: "login", pathMatch: "full" },
-    {path: "login", component: LoginComponent},
-    {path: "users", component: UsersComponent},
-    {path: "**", redirectTo: "login"}
-];
+const routes: Routes = [
+        {path: "", redirectTo: "login", pathMatch: "full"},
+        {path: "login", component: LoginComponent},
+        {path: "users", component: UsersComponent},
+        {path: "**", redirectTo: "login"}
+    ]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
