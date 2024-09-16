24417420f7bc0923882621a5af1cb12a16641b2d
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-09-16 13:06:31 -0400

    update 18 continuing debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { FormsModule } from '@angular/forms';
 import { UserSingleComponent } from './users/user-single/user-single.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+// Import the HttpClientModule and provide the HttpClient interceptor
+
 
 
 @NgModule({
@@ -14,15 +18,17 @@ import { LoginComponent } from './auth/login/login.component';
     AppComponent,
     UsersComponent,
     UserSingleComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { UsersComponent } from "./users/users.component";
+// Removed the import statement for AppRoutingModule
 
 const routes: Routes = [
+    
+    // Add routes for other components here...
+    { path: '', redirectTo: "login", pathMatch: "full" },
     {path: "login", component: LoginComponent},
-    {path: "users", component: LoginComponent},
-    {path: "**", component: LoginComponent}
+    {path: "users", component: UsersComponent},
+    {path: "**", redirectTo: "login"}
 ];
 
 @NgModule({
-    imports: [],
-    exports: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
