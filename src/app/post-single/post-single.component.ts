103e9ed247c6da0df47929c5fdb4803382b9013f
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-10-07 12:06:24 -0400

    update 58 post single

--- src/app/post-single/post-single.component.ts
+++ src/app/post-single/post-single.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import * as PostModel from '../models/Post.model';
+import { PostService } from '../services/post-service.service';
+import { UserService } from '../services/user-service.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-post-single',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './post-single.component.css'
 })
 export class PostSingleComponent {
-
+  @Input() post: PostModel.Post;
+  datePipe: DatePipe = new DatePipe("en-US"); 
+  
+  constructor(
+    public postService: PostService,
+    public userService: UserService,
+  ) {
+    this.post = {...this.postService.emptyPost};
+  }
+  setDateForDisplay(dateToConvert: Date | string){
+    console.log(typeof dateToConvert);
+    if (typeof dateToConvert === 'string') {
+      dateToConvert = new Date(Date.parse(dateToConvert));
+    }
+    return this.datePipe.transform(dateToConvert, "MM/DD/YYYY hh:mm:ss a");
+  }
 }
